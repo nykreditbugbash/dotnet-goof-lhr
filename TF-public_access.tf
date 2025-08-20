b2fc64af862fc371fb8ba15d68c57a921ab4e9e6
LouiseTegner <50576907+LouiseTegner@users.noreply.github.com>
2025-08-20 12:28:17 +0200

    Update TF-public_access.tf

--- TF-public_access.tf
+++ TF-public_access.tf
@@ -2,6 +2,7 @@ resource "aws_db_instance" "db" {
   publicly_accessible  = false
   allocated_storage    = 20
   storage_type         = "gp2"
+  storage_encrypted    = true
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
@@ -9,6 +10,7 @@ resource "aws_db_instance" "db" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
+  backup_retention_period = 1
 }
 
 resource "aws_dms_replication_instance" "db" {
